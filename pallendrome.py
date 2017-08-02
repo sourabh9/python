cc34f2423a0b1aed2b7c1f62616a73eaf487bac4
sourabh <sourabhgoal@gmail.com>
2017-08-02 13:31:51 +0530

    Second method

--- pallendrome.py
+++ pallendrome.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+####First Method
 abc = raw_input("Enter your string: ")
 l=len(abc)-1
 i =0
@@ -10,10 +11,28 @@ for c in abc:
     else :
         f =1
         break
-
 if f ==0 :
     print "string is pallendrome"
 if f==1 :
     print "string is not pallendrome"
 
+###Second Method
+f =0
+def pallendrome(str11):
+    l=len(str11)-1
+    i =0
+    global f
+    for c in str11:
+        if c == str11[l-i] :
+            i = i+1
+        else :
+            f =1
+            break
+    return f
 
+a = raw_input("Enter the string: ")
+result = pallendrome(a)
+if result ==1 :
+    print "Not pallendrome"
+else :
+    print "Pallendrome"
