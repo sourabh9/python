a7ed49bce8bf8929fcadf4881e9969b5e40000c2
sourabh9 <sourabhgoal@gmail.com>
2017-08-03 18:44:27 +0530

    python 3.4 porting
    Verison python 3.4 test

--- agename.py
+++ agename.py
@@ -5,13 +5,13 @@
 #
 
 x = "Welcome"
-print x[::-1]
-print x[-2:-4:-1]
-print x[0:6:4]
+print ( x[::-1])
+print ( x[-2:-4:-1])
+print ( x[0:6:4])
 
 a = [5,10,15,20,25,30,35,40]
-print a[3:0:-1]
-print a[::-1]
-print a[:0:-1]
-print a[2::-1]
-print a[1::4]
+print ( a[3:0:-1])
+print ( a[::-1])
+print ( a[:0:-1])
+print ( a[2::-1])
+print ( a[1::4]) 
